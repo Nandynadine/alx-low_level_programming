bd756727d776ef49008fc427b1f95e5c99edd1cd
Nadine Nizere <nizerenadine1@gmail.com>
2023-02-20 10:14:56 -0800

    While and if statement

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - print lowercase alpha a-z but remove 'q' and 'e'
- *Return: Always 0 (Success)
+ * main-Entry point
+ *
+ * description: Print all alphabets except q and e
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
