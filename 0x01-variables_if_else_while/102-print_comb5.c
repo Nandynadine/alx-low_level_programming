ada5332571ebc2a6c82ff0d378b5b89e18cd7f9f
Nadine Nizere <nizerenadine1@gmail.com>
2023-02-20 12:51:39 -0800

    For statement and 2 combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,29 +6,26 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int num1, num2;
+	int i, j;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 98 && num2 == 99)
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if ((i == 98) && (j == 99))
 				continue;
-
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
