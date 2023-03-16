b3a5ef130619ccfa54dd37325e08ead0cd6b125a
Nadine Nizere <nizerenadine1@gmail.com>
2023-03-16 12:25:09 -0700

    main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Nadine Nizere
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
